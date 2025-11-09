538bb79b965fffcfe4b360ffa3dce6c53f640d93
Konstantin Epishin <konstantinepishin@icloud.com>
2025-11-09 23:53:15 +0300

    Improve script for cleaning project

--- watch-compile.sh
+++ watch-compile.sh
@@ -4,4 +4,5 @@ watchexec \
   --watch ./src/ \
   --exts tex,bib,sty,cls \
   --restart \
-  -- latexmk -pdf -interaction=nonstopmode -output-directory=dist ./src/main.tex
+  -- 'latexmk -pdf -dvi- -ps- -interaction=nonstopmode -output-directory=dist -aux-directory=dist-aux ./src/main.tex && \
+      latexmk -c -output-directory=dist ./src/main.tex'
